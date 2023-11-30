0f9036c7bed913605ffaf766c5f535e4094a1bb5
skyler <skylerjkinney@gmail.com>
2023-11-30 15:10:25 -0700

    Merge the minds

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const heroes = [
     {
         name: 'Josh Demek',
         type: 'Baller',
+        picture: '🧛',
         damage: 10,
-        health: 100
+        health: 100,
+        maxHealth: 100,
         
     },
     {
         name: 'Skyler Seawind',
-        type: 'elf',
+        type: 'Alpha',
+        picture: '🦸',
         damage: 5,
-        health: 100
+        health: 100,
+        maxHealth: 100,
     }
 ]
 
@@ -23,20 +27,37 @@ const boss = {
     maxHealth: 100,
     damage: 5,
     level: 1,
-    gold: 50
+    gold: 50,
 }
 
 function attack(){
+    healthCheck()
     heroes.forEach(hero => {
         hero.health -= boss.damage
         boss.health -= hero.damage
-        console.log(boss.health, '🐌', hero.health, '🦸')
+        console.log(boss.health, '🐌', hero.health, hero.picture)
     })
+}
+
+
+ function healthCheck(){
     if(boss.health <= 0){
         boss.maxHealth += 50
         boss.damage += 5
         boss.level += 1
         boss.gold += 50
         boss.health = boss.maxHealth
+        levelUp()
     }
-}
+ }
+
+function levelUp() { 
+    heroes.forEach(hero => {
+        hero.maxHealth += 15,
+        hero.damage += 5,
+        console.log(hero)
+        playerGold += boss.gold
+        hero.health = hero.maxHealth
+    })
+    window.alert('LEVEL UP')
+    }
